2d3d86afa45bd6171cdd7dfab83c254d7b9f657c
SAMYUKTHA KRP <krpsamyuktha2002@gmail.com>
2026-05-25 21:08:43 +0530

    Transforming and using response data

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -18,9 +19,9 @@ export class AvailablePlacesComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
-    const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places').subscribe({
-      next: (resData) => {
-        console.log(resData.places);
+    const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places').pipe(map((resData) => resData.places)).subscribe({
+      next: (places) => {
+        this.places.set(places);
       }
     });
     this.destroyRef.onDestroy(() => {
